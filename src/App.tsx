b51b8c72fee0b1a80c8b19da9c2ee7fb529db9b1
sudhakar <sudhakr.balla@gmail.com>
2024-02-20 21:25:22 +0530

    Initial draft after creating project with typescript template

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+import Login from './components/login';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+      <Login />
+  );
+}
+
+export default App;
