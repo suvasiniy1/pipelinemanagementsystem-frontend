b2ab72943675884b9ea0a92d92dd5cb33b001962
Pankur Patel <pankur83012@gmail.com>
2024-03-04 18:28:17 +0530

    deals dahsboard html structure created

--- src/components/pipeline/deal/deals.tsx
+++ src/components/pipeline/deal/deals.tsx
@@ -2,7 +2,181 @@ import React from "react";
 
 export const Deals = () => {
     return (
-       <h3>Deals comming soon</h3>
+      
+        <div className="pdstage-area">
+            <div className="container-fluid">
+            <div className="pdstage-row">
+                <div className="pdstage-col">
+                    <div className="pdstage-innercol">
+                        <div className="pdstage-header">
+                            <div className="pdstage-head">New Lead</div>
+                            <div className="pdstage-summary">
+                                <div className="pdstage-value">
+                                    <span className='pdstage-price'>£0</span><span className='pdstage-num'>6 deals</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='pdstage-list'>
+                        <div className="pdstage-item">
+                            <div className='pdstage-box'>
+                            <a className='pdstage-boxlink' href=''>
+                                <div className="pdstage-title">Bokani Tshuma</div>
+                                <div className="pdstage-description">
+                                    <div className="pdstage-descitem">Esteem care, Bokani Tshuma</div>
+                                </div>                           
+                                <div className="pdstage-status-row">
+                                    <div className="pdstage-avatar">
+                                        <i className="rs-icon rs-icon-user-circle"></i>
+                                    </div>
+                                    <div className="pdstage-value">
+                                        <span>£0</span>
+                                    </div>                                
+                                </div>
+                            </a>
+                            <div className="pdstage-status-indicator">
+                                <div className='pdstage-indicator-icon'><i className="rs-icon rs-icon-arrow-circle-left"></i></div>
+                            </div>
+                            </div>
+                        </div>
+
+                        <div className="pdstage-item">
+                            <div className='pdstage-box'>
+                            <a className='pdstage-boxlink' href=''>
+                                <div className="pdstage-title">Lem Deng Stok</div>
+                                <div className="pdstage-description">
+                                    <div className="pdstage-descitem">Esteem care, Bokani Tshuma</div>
+                                </div>                           
+                                <div className="pdstage-status-row">
+                                    <div className="pdstage-avatar">
+                                        <i className="rs-icon rs-icon-user-circle"></i>
+                                    </div>
+                                    <div className="pdstage-value">
+                                        <span>£0</span>
+                                    </div>                                
+                                </div>
+                            </a>
+                            <div className="pdstage-status-indicator">
+                                <div className='pdstage-indicator-icon'><i className="rs-icon rs-icon-arrow-circle-left"></i></div>
+                            </div>
+                            </div>
+                        </div>
+
+
+                        </div>
+                        <div className="pdstage-add"><button className='pdstage-addplus'><i className="rs-icon rs-icon-plus"></i></button></div>
+                    </div>
+                </div>
+
+                <div className="pdstage-col">
+                    <div className="pdstage-innercol">
+                        <div className="pdstage-header">
+                            <div className="pdstage-head">1st Call</div>
+                            <div className="pdstage-summary">
+                                <div className="pdstage-value">
+                                    <span className='pdstage-price'>£0</span>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div className="pdstage-add"><button className='pdstage-addplus'><i className="rs-icon rs-icon-plus"></i></button></div>
+                    </div>
+                </div>
+
+                <div className="pdstage-col">
+                    <div className="pdstage-innercol">
+                        <div className="pdstage-header">
+                            <div className="pdstage-head">2nd Call</div>
+                            <div className="pdstage-summary">
+                                <div className="pdstage-value">
+                                    <span className='pdstage-price'>£0</span>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div className="pdstage-add"><button className='pdstage-addplus'><i className="rs-icon rs-icon-plus"></i></button></div>
+                    </div>
+                </div>
+
+                <div className="pdstage-col">
+                    <div className="pdstage-innercol">
+                        <div className="pdstage-header">
+                            <div className="pdstage-head">3rd Call</div>
+                            <div className="pdstage-summary">
+                                <div className="pdstage-value">
+                                    <span className='pdstage-price'>£0</span>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div className="pdstage-add"><button className='pdstage-addplus'><i className="rs-icon rs-icon-plus"></i></button></div>
+                    </div>
+                </div>
+
+                <div className="pdstage-col">
+                    <div className="pdstage-innercol">
+                        <div className="pdstage-header">
+                            <div className="pdstage-head">4th Call</div>
+                            <div className="pdstage-summary">
+                                <div className="pdstage-value">
+                                    <span className='pdstage-price'>£0</span>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div className="pdstage-add"><button className='pdstage-addplus'><i className="rs-icon rs-icon-plus"></i></button></div>
+                    </div>
+                </div>
+
+                <div className="pdstage-col">
+                    <div className="pdstage-innercol">
+                        <div className="pdstage-header">
+                            <div className="pdstage-head">Final Call</div>
+                            <div className="pdstage-summary">
+                                <div className="pdstage-value">
+                                    <span className='pdstage-price'>£0</span>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div className="pdstage-add"><button className='pdstage-addplus'><i className="rs-icon rs-icon-plus"></i></button></div>
+                    </div>
+                </div>
+
+                <div className="pdstage-col">
+                    <div className="pdstage-innercol">
+                        <div className="pdstage-header">
+                            <div className="pdstage-head">Appointment Booked</div>
+                            <div className="pdstage-summary">
+                                <div className="pdstage-value">
+                                    <span className='pdstage-price'>£0</span>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div className="pdstage-add"><button className='pdstage-addplus'><i className="rs-icon rs-icon-plus"></i></button></div>
+                    </div>
+                </div>
+
+                <div className="pdstage-col">
+                    <div className="pdstage-innercol">
+                        <div className="pdstage-header">
+                            <div className="pdstage-head">CLOSED LEADS</div>
+                            <div className="pdstage-summary">
+                                <div className="pdstage-value">
+                                    <span className='pdstage-price'>£0</span>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div className="pdstage-add"><button className='pdstage-addplus'><i className="rs-icon rs-icon-plus"></i></button></div>
+                    </div>
+                </div>
+
+                
+
+            </div>
+            </div>
+        </div>
     );
 }
 
