15e17f8cc7da4262cdd48f320dac612fef5c6498
Pankur Patel <108931457+pankurpatel@users.noreply.github.com>
2024-03-07 23:04:56 +0530

    header buttons

--- src/components/pipeline/deal/dealHeader.tsx
+++ src/components/pipeline/deal/dealHeader.tsx
@@ -4,6 +4,10 @@ import { useNavigate } from 'react-router-dom';
 import { DealAddEditDialog } from './dealAddEditDialog';
 import SelectDropdown from '../../../elements/SelectDropdown';
 import { PipeLine } from '../../../models/pipeline';
+import { width } from '@xstyled/styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretDown, faPencil, faChartSimple, faAlignCenter, faBars, faDollarSign} from '@fortawesome/free-solid-svg-icons';
+
 
 
 type params = {
@@ -22,7 +26,7 @@ export const DealHeader = (props: params) => {
     const addorUpdateStage = () => {
         return (
             <>
-                <button type="button" className="btn btn-primary" onClick={(e: any) => navigate("/pipeline/edit")}> <SettingsIcon /></button>
+                <button type="button" className="btn" onClick={(e: any) => navigate("/pipeline/edit")}> <FontAwesomeIcon icon={faPencil} /></button>
             </>
         )
     }
@@ -30,7 +34,7 @@ export const DealHeader = (props: params) => {
     const addorUpdateDeal = () => {
         return (
             <>
-                <button type="button" className="btn btn-success" onClick={(e: any) => setDialogIsOpen(true)} disabled={!canAddDeal}>+ Add Deal</button>
+                <button type="button" className="btn btn-success" onClick={(e: any) => setDialogIsOpen(true)} disabled={!canAddDeal}>+ Deal</button>
             </>
         )
     }
@@ -40,29 +44,42 @@ export const DealHeader = (props: params) => {
             <div className="pipe-toolbar pt-3 pb-4">
                 <div className="container-fluid">
                     <div className="row">
-                        <div className="col-sm-8 toolbarview-actions">
-
-                            {addorUpdateDeal()}
-                        </div>
-                        <div className="col-sm-4 toolbarview-filters">
-                            <div className="row">
-                                <div className='col-sm-8 toolbarview-filtersrow'>
-                                    <select className="form-control"
-                                            value={selectedItem?.pipelineID}
-                                            key={"region"}
-                                            onChange={(e: any) => setSelectedItem(e.target.value)}
-                                    >
-                                        <option value="">Select</option>
-                                        {pipeLinesList?.map((item: PipeLine, index: number) => {
-                                            return (
-                                                <option key={index} value={item.pipelineID}>{item.pipelineName}</option>
-                                            );
-                                        }
-                                        )}
-                                    </select>
+                        <div className="col-sm-3 toolbarview-actions">
+                            <div className='toolbarview-actionsrow'>
+                                <div className="d-flex toolbutton-group">
+                                    <button className="toolpipebtn" type="button"><FontAwesomeIcon icon={faChartSimple} /></button>
+                                    <button className="tooldealbtn" type="button"><FontAwesomeIcon icon={faBars} /></button>
+                                    <button className="tooltimebtn" type="button"><FontAwesomeIcon icon={faDollarSign} /></button>
                                 </div>
-                                <div className='col-sm-2 toolbarview-filtersrow'>
-                                    {addorUpdateStage()}
+                                {addorUpdateDeal()}
+                            </div>
+                        </div>
+                        <div className="col-sm-5 toolbarview-summery">
+                        
+                        </div>
+                        <div className="col-sm-4 toolbarview-filters d-flex">
+                            <select className="form-control"
+                                    value={selectedItem?.pipelineID}
+                                    key={"region"}
+                                    onChange={(e: any) => setSelectedItem(e.target.value)}
+                            >
+                                <option value="">Select</option>
+                                {pipeLinesList?.map((item: PipeLine, index: number) => {
+                                    return (
+                                        <option key={index} value={item.pipelineID}>{item.pipelineName}</option>
+                                    );
+                                }
+                                )}
+                            </select>                                
+                            <div className='toolbarview-filtersrow'>                                    
+                                <div className="pipeselectbtngroup">
+                                    <div className='pipeselectbox variantselectbox'>
+                                        <button className="pipeselect" type="button"><FontAwesomeIcon icon={faChartSimple} /> Workington <FontAwesomeIcon icon={faCaretDown} /></button>
+                                    </div>
+                                    <div className='updatestagebtn'>{addorUpdateStage()}</div>
+                                </div>
+                                <div className="pipeselectbox selecteveryonebox">
+                                    <button className="pipeselect" type="button"><FontAwesomeIcon icon={faAlignCenter} /> Everyone <FontAwesomeIcon icon={faCaretDown} /></button>
                                 </div>
                             </div>
                         </div>
