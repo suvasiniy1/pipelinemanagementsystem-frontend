17c27e3032c6e01d0fdc77cac31105dafcc8d100
Pankur Patel <108931457+pankurpatel@users.noreply.github.com>
2024-08-30 09:53:39 +0530

    Fetch and commit

--- public/config.js
+++ public/config.js
@@ -1,5 +1,6 @@
 window.config = {
-    ServicesBaseURL: "http://20.26.122.140/PLMS/api",
+    //ServicesBaseURL: "http://localhost:5127/api",
+    ServicesBaseURL: "http://20.26.122.140/PLMS/api", 
     DefaultStages:["Qualified", "Conact Made", "Demo Scheduled", "Proposal Made", "Negotiations Started"],
     UseMockService:false,
     CampaignSections:["Assets", "Tasks"],
