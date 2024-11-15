9694fd8dee1c49dd3f73a8d630cb9c58e542dbce
suvas <suvas@SUVASINIK>
2024-11-15 14:59:22 +0000

    confirm email issue fixed

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
 window.config = {
    // ServicesBaseURL: "http://localhost:5127/api",
-    ServicesBaseURL: "http://20.26.122.140/PLMS/api",
+   ServicesBaseURL: "http://20.26.122.140/PLMS/api",
     DefaultStages: [
       "Qualified",
       "Conact Made",
@@ -31,6 +31,9 @@ window.config = {
           "Admin",
           "Reporting",
           "Enquiries",
+          "Login",
+          "Home",
+          "confirm-email",
         ],
       },
       {
