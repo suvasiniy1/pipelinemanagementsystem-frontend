361f241fbac97e7424b4ac06868c31d2404d0b4a
Pankur Patel <108931457+pankurpatel@users.noreply.github.com>
2025-01-24 13:36:53 +0530

    filter cion updated

--- src/components/pipeline/deal/dealHeader.tsx
+++ src/components/pipeline/deal/dealHeader.tsx
@@ -27,6 +27,7 @@ import Dropdown from "react-bootstrap/Dropdown";
 import FilterDropdown from "./dealFilters/filterDropdown/filterDropdown";
 import { DealFilter } from "../../../models/dealFilters";
 import FilterAltIcon from '@mui/icons-material/FilterAlt';
+import FilterAltOffIcon from '@mui/icons-material/FilterAltOff';
 
 type params = {
   canAddDeal: boolean;
@@ -226,7 +227,7 @@ export const DealHeader = (props: params) => {
                   </div>
                 </div>
                 <div className="pipefilterbtn">
-                  <div className="filterbtn"><a className="btn" href="javascript:void(0);" onClick={(e:any)=>setSelectedFilterObj(null)}><FilterAltIcon /></a></div>
+                  <div className="filterbtn"><a className="btn" href="javascript:void(0);" onClick={(e:any)=>setSelectedFilterObj(null)}><FilterAltOffIcon /></a></div>
                 </div>
 
                 {/* <div className="pipeselectbox selecteveryonebox">
